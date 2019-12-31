1dfe0aa0e72398c9d38a0705bae5ae5ad2e5b56b
Toma Nistor <toma.nistor@fishawack.com>
2019-12-30 19:50:19 -0800

    Added gradient background to play scene

--- src/scripts/scenes/play.js
+++ src/scripts/scenes/play.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import config from '../config';
 
 let player,
     score,
@@ -15,16 +16,29 @@ export default {
     }
   },
 
-  init: function () {
+  init: function() {
     gameOver = false;
     score = 0;
   },
 
-  create: function () {
+  create: function() {
+    // Add: Gradient Background
+    const texture = this.textures.createCanvas('gradient', config.width, config.height);
+    const context = texture.getContext();
+    const gradient = context.createLinearGradient(0, 0, 0, config.height);
 
+    gradient.addColorStop(0, '#CA52D8');
+    gradient.addColorStop(1, '#B292DE');
+
+    context.fillStyle = gradient;
+    context.fillRect(0, 0, config.width, config.height);
+
+    texture.refresh();
+
+    this.add.image(600, 300, 'gradient');
   },
 
-  update: function () {
+  update: function() {
     if (gameOver) {
       this.scene.stop().run('end');
 
