371c16d8364a931c58be75e45bb00e1e1eb610a0
Toma Nistor <toma.nistor@fishawack.com>
2020-01-04 21:49:17 -0800

    Created palms array and started moving palms

--- src/scripts/scenes/play.js
+++ src/scripts/scenes/play.js
@@ -6,6 +6,25 @@ let player,
     score,
     gameOver;
 
+let palms = [];
+
+function movePalms(palms) {
+  palms.forEach(function(palm) {
+    palm.x -= 5;
+    console.log('first ' + this);
+
+    if (palm.x < -300) {
+      console.log('second ' + this);
+      console.log('third' + palm);
+      this.resetPalm(palm);
+    }
+  });
+}
+
+function resetPalm(palm) {
+  palm.x = config.width;
+}
+
 export default {
   key: 'play',
 
@@ -24,32 +43,32 @@ export default {
     score = 0;
   },
 
-  preload: function() {
-    // Add Image: Gradient
-    this.add.image(600, 300, 'gradient');
-
-    // Add Image: Horizontal Lines
-    this.add.image(600, 300, 'horizontalLines');
-
-    // Add Images: Palm Trees
-    this.add.image(100, helpers.randomInteger(80, 175), 'palm1')
-      .setOrigin(0)
-      .setScale(helpers.randomNumber(.55, .90));
-    this.add.image(300, helpers.randomInteger(80, 175), 'palm2')
-      .setOrigin(0)
-      .setScale(helpers.randomNumber(.55, .90));
-    this.add.image(500, helpers.randomInteger(80, 175), 'palm3')
-      .setOrigin(0)
-      .setScale(helpers.randomNumber(.55, .90));
-    this.add.image(700, helpers.randomInteger(80, 175), 'palm4')
-      .setOrigin(0)
-      .setScale(helpers.randomNumber(.55, .90));
-  },
-
   create: function() {
     this.add.text(config.width * .5, config.height * .5, 'Game Will Go Here\n\nPress (E) for Game Over', {
       align: 'center'
     }).setOrigin(.5);
+
+    // Add Image: Gradient
+    this.add.image(600, 300, 'gradient');
+
+    // Add Image: Horizontal Lines
+    // this.add.image(600, 300, 'horizontalLines');
+
+    // Add Images: Palm Trees
+    palms[0] = this.add.sprite(0, helpers.randomInteger(110, 175), 'palm1')
+      .setOrigin(0)
+      .setScale(helpers.randomNumber(.60, .90));
+    palms[1] = this.add.sprite(300, helpers.randomInteger(110, 175), 'palm2')
+      .setOrigin(0)
+      .setScale(helpers.randomNumber(.60, .90));
+    palms[2] = this.add.sprite(600, helpers.randomInteger(110, 175), 'palm3')
+      .setOrigin(0)
+      .setScale(helpers.randomNumber(.60, .90));
+    palms[3] = this.add.sprite(900, helpers.randomInteger(110, 175), 'palm4')
+      .setOrigin(0)
+      .setScale(helpers.randomNumber(.60, .90));
+
+    console.log(palms)
 
     // Next Scene: Menu
     this.input.keyboard.on('keydown-E', function() {
@@ -59,6 +78,8 @@ export default {
   },
 
   update: function() {
+    movePalms(palms);
+
     if (gameOver) {
       this.scene.stop().run('end');
 
